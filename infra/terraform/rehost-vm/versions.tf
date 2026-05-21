c33b8d540bb1470f0e9f7ebd2740f2583bed5ad0
ravik <ravik.kotapati@gmail.com>
2026-05-21 22:51:06 +0800

    Disable AzureRM auto provider registration

--- infra/terraform/rehost-vm/versions.tf
+++ infra/terraform/rehost-vm/versions.tf
@@ -14,7 +14,8 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = var.subscription_id
+  subscription_id                 = var.subscription_id
+  resource_provider_registrations = "none"
 
   features {}
 }
